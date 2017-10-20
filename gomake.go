49747a5c99003ef049aadb3ce3a5cae293805850
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-10-20 17:58:06 +0200

    Execute targets

--- gomake.go
+++ gomake.go
@@ -8,6 +8,7 @@ import (
     "errors"
     "io"
     "bufio"
+    "os/exec"
 )
 
 const CommandPrefix = "\t"
@@ -31,7 +32,10 @@ type Rules map[string]*Rule
 
 func help() {
     fmt.Println("Help:")
-    fmt.Println("gomake path-to-makefile")
+    fmt.Println("\tgomake path-to-makefile rule-to-execute")
+    fmt.Println("\nExamples:")
+    fmt.Println("\tgomake Makefile all")
+    fmt.Println("\tgomake ../MyMakefile test.c")
 }
 
 func getLineType(line string) int {
@@ -104,8 +108,34 @@ func printRules(rules *Rules) {
     }
 }
 
+func getDependentTargets(rule *Rule, rules *Rules) (dependencies []*Rule) {
+    for _, dep := range rule.Dependencies {
+        if r, isPresent := (*rules)[dep]; isPresent { // The dependency is a target itself
+            dependencies = append(dependencies, r)
+        }
+    }
+    return
+}
+
+func execute(target string, rules *Rules) (err error) {
+    rule := (*rules)[target]
+    dependencies := getDependentTargets(rule, rules)
+
+    for _, dep := range dependencies {
+        execute(dep.Target, rules)
+    }
+
+    for _, cmd := range rule.Commands {
+        if e := exec.Command("sh", "-c", cmd).Run(); e != nil {
+            return e
+        }
+    }
+
+    return
+}
+
 func main() {
-    if len(os.Args) < 2 {
+    if len(os.Args) < 3 {
         fmt.Println("Not enough arguments")
         help()
         os.Exit(1)
@@ -131,7 +161,16 @@ func main() {
         os.Exit(1)
     }
 
-    printRules(&rules)
+    target := os.Args[2]
+    if _, isPresent := rules[target]; !isPresent {
+        fmt.Printf("The rule '%s' does not appear in the Makefile\n", target)
+        os.Exit(1)
+    }
+
+    if e := execute(target, &rules); e != nil {
+        fmt.Printf("Error executing target '%s': %s\n", target, e)
+        os.Exit(1)
+    }
 
     f.Close()
 }
