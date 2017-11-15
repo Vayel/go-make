5ddff94649c36ef068a96fe8ed0b24c7d6b0d5d9
Sanhaji Omar <omar.sanhaji@grenoble-inp.org>
2017-11-15 12:32:36 -0300

    FIXME: automating launch of master and slaves

--- deployment.sh
+++ deployment.sh
@@ -11,15 +11,26 @@
 
 kadeploy3 -f $OAR_FILE_NODES -e wheezy-x64-nfs -k
 
-taktuk -l root -s -o connector -o status -o output='"$host: $line\n"' -f $OAR_FILE_NODES broadcast exec [ "apt-get update" ]
+uniq $OAR_FILE_NODES | taktuk -l root -s -o connector -o status -o output='"$host: $line\n"' -f - broadcast exec [ "apt-get update" ]
 
-taktuk -l root -s -o connector -o status -o output='"$host: $line\n"' -f $OAR_FILE_NODES broadcast exec [ "apt-get install golang-go unzip git -y" ]
+uniq $OAR_FILE_NODES | taktuk -l root -s -o connector -o status -o output='"$host: $line\n"' -f - broadcast exec [ "apt-get install golang-go unzip git -y" ]
 
-taktuk -s -o connector -o status -o output='"$host: $line\n"' -f $OAR_FILE_NODES broadcast exec [ "wget https://github.com/Vayel/go-make/archive/master.zip" ]
+uniq $OAR_FILE_NODES | taktuk -s -o connector -o status -o output='"$host: $line\n"' -f - broadcast exec [ "wget https://github.com/Vayel/go-make/archive/master.zip" ]
 
-taktuk -s -o connector -o status -o output='"$host: $line\n"' -f $OAR_FILE_NODES broadcast exec [ "unzip master.zip" ]
+uniq $OAR_FILE_NODES | taktuk -s -o connector -o status -o output='"$host: $line\n"' -f - broadcast exec [ "unzip master.zip" ]
 
-taktuk -s -o connector -o status -o output='"$host: $line\n"' -f $OAR_FILE_NODES broadcast exec [ "cd go-make-master && make" ]
+uniq $OAR_FILE_NODES | taktuk -s -o connector -o status -o output='"$host: $line\n"' -f - broadcast exec [ "cd go-make-master && make" ]
+
+head -1 $OAR_FILE_NODES | taktuk -m - broadcast exec [ nohup ./go-make-master/bin/master makefiles/10 all 10000 outputfiles/ ]
+
+taktuk -m "$(head -1 $OAR_FILE_NODES)" broadcast exec [ "nohup ./go-make-master/bin/master go-make-master/makefiles/10 all 10000 go-make-master/outputfiles/" ] &
+
+#nodes= uniq $OAR_FILE_NODES
+#master= head -1 $OAR_FILE_NODES
+#for node in $nodes
+#do
+#    taktuk -m node broadcast exec [ "nohup ./slave $master 10000 $node 40000 ~/go-make-master/outputfiles" ] &
+#done
 
 #To see the machines: cat $OAR_NODES_FILES
 #To get the IP addresses: cat .ssh/known_hosts | grep <machine_name>
