1ae05758647126d4ac88366a544997036b568b45
PiggyGenius <ludo.itrts@gmail.com>
2017-11-19 18:31:48 +0000

    Make test file take number of slave in arg

--- launch_slave.sh
+++ launch_slave.sh
@@ -9,12 +9,10 @@
 
 export UNIQ_FILE_NODES=/home/lcarre/grid5000_nodes.txt
 master=$(head -1 $UNIQ_FILE_NODES)
-nodes_without_master=$(cat $UNIQ_FILE_NODES | tail -n +2)
-#machine=genepi-14.grenoble.grid5000.fr
+nodes_without_master=$(cat $UNIQ_FILE_NODES | tail -n +2 | head -$1)
 for machine in $nodes_without_master
 do
     taktuk -m $machine broadcast exec [ "/tmp/go-make/bin/slave $master 10000 $machine 40000 /tmp/go-make/outputfiles/" ]
-	#taktuk -m $machine broadcast exec [ "/tmp/go-make/bin/slave $master 10000 $machine 40000 /tmp/go-make/outputfiles/" ]
 done
 
 
