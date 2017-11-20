ab6aac69f75ff606be6976fc6483ebafda722416
PiggyGenius <ludo.itrts@gmail.com>
2017-11-20 16:36:32 +0000

    Remove makefile from launch scripts and add log variable

--- launch_master.sh
+++ launch_master.sh
@@ -7,7 +7,12 @@
 #./nodes_configuration.sh
 #UNIQ_FILE_NODES exported by nodes_configuration.sh
 
+slaveport = 40000
+output = /tmp/go-make/outputfiles/
+logfile = ~/go-make/logs/
+
 export UNIQ_FILE_NODES=~/grid5000_nodes.txt
 master=$(head -1 $UNIQ_FILE_NODES)
-taktuk -m $master broadcast exec [ "make --directory /tmp/go-make/ run_master $1" ] 
+taktuk -m $master broadcast exec [ "/tmp/go-make/bin/master $1 $slaveport $output $logfile" ] 
+#taktuk -m $master broadcast exec [ "make --directory /tmp/go-make/ run_master $1" ] 
 #taktuk -m $master broadcast exec [ "make --directory /tmp/go-make/ run_master makefiles/4" ] 
--- launch_slave.sh
+++ launch_slave.sh
@@ -7,12 +7,17 @@
 #./nodes_configuration.sh
 #UNIQ_FILE_NODES exported by nodes_configuration.sh
 
+slaveport = 40000
+masterport = 10000
+output = /tmp/go-make/outputfiles/
+logfile = ~/go-make/logs/
+
 export UNIQ_FILE_NODES=~/grid5000_nodes.txt
 master=$(head -1 $UNIQ_FILE_NODES)
 nodes_without_master=$(cat $UNIQ_FILE_NODES | tail -n +2 | head -$1)
 for machine in $nodes_without_master
 do
-    taktuk -m $machine broadcast exec [ "/tmp/go-make/bin/slave $master 10000 $machine 40000 /tmp/go-make/outputfiles/" ]
+    taktuk -m $machine broadcast exec [ "/tmp/go-make/bin/slave $master $masterport $machine $slaveport $output $logfile" ]
 done
 
 
