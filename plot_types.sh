10d5a3d65da847cdf88f8a5f43f8c2983295f5ca
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2017-11-21 12:18:16 +0100

    parallelizable

--- plot_types.sh
+++ plot_types.sh
@@ -6,7 +6,7 @@ then
     exit
 fi
 
-python convert_to_gnuplot.py "$1"
+python3 convert_to_gnuplot.py "$1"
 ./plot_measure_type.sh times.png times.txt "Temps d'exécution (ms)"
 ./plot_measure_type.sh speedups.png speedups.txt "Accélération"
 ./plot_measure_type.sh efficiencies.png efficiencies.txt "Efficacité"
