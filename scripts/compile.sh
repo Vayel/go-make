309cc59693b2bfa5235f73d3ee02c7a70707ad1f
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-11-23 21:39:27 +0100

    Fix spaces

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -7,5 +7,5 @@ taktuk -l root -m $node broadcast exec [ "apt-get install make" ]
 
 for prog in "master" "slave" "sequential"
 do
-taktuk -m $node broadcast exec [ "cd $PROJECT_DIR; make $prog; mv bin/$prog $BIN_DIR" ] 
+    taktuk -m $node broadcast exec [ "cd $PROJECT_DIR; make $prog; mv bin/$prog $BIN_DIR" ] 
 done
