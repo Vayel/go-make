a21108da2f57352a89a52b0b30c4c02310e58237
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-11-23 23:22:52 +0100

    Fix check_nodes

--- scripts/check_nodes.sh
+++ scripts/check_nodes.sh
@@ -6,13 +6,14 @@ nodes=$(cat $UNIQ_NODES_FILE)
 > $NODES_FILE
 for node in $nodes
 do
-    ret=$(taktuk -o status -m $node broadcast exec [ "ls" ])
-    if [[ -z $ret ]]
+    output=$(taktuk -o taktuk -o status -o connector -o error='"Error: $line\n"' -m $node broadcast exec [ "go help" ])
+    if [[ $output == Error:* ]]
     then
-        echo $ret >> $NODES_FILE
+        echo $output
+    else
+        echo $node >> $NODES_FILE
     fi
 done
 
-echo "Available nodes:"
-cat $NODES_FILE | wc -l
+echo -e "\nAvailable nodes: $(cat $NODES_FILE | wc -l)"
 cat $NODES_FILE
